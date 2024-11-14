012a8cbc307e1ea5acf132744aed1f12bc0235e1
Jason <jayz71189@gmail.com>
2024-11-14 18:16:02 -0500

    upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import harvardArt from "./data/harvardArt";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <h2>Harvard Art Museum</h2>
+        <p>
+          Look, but Don&apos;t Touch. Please select a Gallery in the navigation
+          bar.
+        </p>
+      </>
+    ),
+  },
+  { path: "*", element: <h2>Page Not Found</h2> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
